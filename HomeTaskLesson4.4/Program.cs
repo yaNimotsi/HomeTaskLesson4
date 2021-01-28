65f19db232ca2cde0fd8fc53bcddf8939458bfaf
yaNimotsi <yanimotsi@yanmdex.ru>
2021-01-28 22:34:41 +0300

    Create task4

---
+++ HomeTaskLesson4.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeTaskLesson4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nMax = GetMaxNum();
+
+            Console.WriteLine($"Получено значение = {Fibonachi(nMax, 0)}");
+        }
+
+        static int GetMaxNum()
+        {
+            Console.WriteLine("Введите максимальный номер для вычисления значения");
+            int nMax;
+            return int.TryParse(Console.ReadLine(), out nMax) ? nMax : 0;
+        }
+
+        static int Fibonachi(int iMax, int i)
+        {
+
+            if (i == iMax)
+            {
+                return i;
+            }
+            return i + Fibonachi(iMax, ++i);
+        }
+    }
+}
